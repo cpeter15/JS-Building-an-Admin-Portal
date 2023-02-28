35e8d0e12b42514bb4c2db8466458e0577ad6107
cpeter15 <caleb.j.peterson9687@gmail.com>
2023-02-27 20:52:08 -0500

    added PATCH fetch

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,42 @@
 
-// Your Code Here
+async function main() {
+
+    let respone = await fetch('http://localhost:3001/listBooks')
+
+    let books = await respone.json()
+
+    books.forEach(displayBook)
+}
+
+function displayBook(book) {
+
+    let root = document.querySelector('#root')
+
+    let list = document.createElement('li')
+    list.textContent = book.title
+
+    let updateInput = document.createElement('input')
+    updateInput.value = book.quantity
+
+    let submitButton = document.createElement('button')
+    submitButton.textContent = "Update"
+
+    submitButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: updateInput.value
+            })
+        })
+    })
+
+    list.append(updateInput, submitButton)
+
+    root.append(list)
+}
+
+main();
